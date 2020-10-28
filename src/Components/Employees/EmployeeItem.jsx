7d64353ca995f892d94ea03f709f41019b797a4e
Lexx <joncanales2@gmail.com>
2020-10-28 18:03:36 -0400

    employee items are wrapped in a border with spacing, now working on incorporating search or categorization functionality.

--- src/Components/Employees/EmployeeItem.jsx
+++ src/Components/Employees/EmployeeItem.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 const EmployeeItem = ({employee: {name, email, picture, phone}}) => {
     return (
-        <div className="row">
-            <img className="col-2 round-img" src={picture.thumbnail} alt="" style={{ width: "40px" }}/>
+        <div className="row my-3 py-2 border border-secondary">
+            <div className="col-2 d-flex align-content-center">
+                <div className="row">
+                    <div className="col-6"></div>
+                    <div className="col-6">            
+                    <img className="rounded " src={picture.thumbnail} alt="" style={{ width: "50px" }}/>
+                    </div>
+                    <div className="col-3"></div>
+                </div>
+            </div>
+
             <div className="col-3"><h4>{name.first} {name.last}</h4></div>
             <div className="col-3"><h4>{phone}</h4></div>
             <div className="col-3"><h4><a href={email}>{email}</a></h4></div>
